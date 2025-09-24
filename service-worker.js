cf2d31562d3c326efc26bd6a9e346b0f5cfe0389
Zuk8829 <zucchelli.mirko@yahoo.it>
2025-09-24 08:15:09 +0200

    Add files via upload
    Primo caricamento

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'sim-manager-v1';
+const urlsToCache = [
+    'gestione_sim.html',
+    'manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aperta');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Ritorna la risorsa dalla cache se disponibile
+                if (response) {
+                    return response;
+                }
+                // Altrimenti, recupera la risorsa dalla rete
+                return fetch(event.request);
+            })
+    );
+});
+
+// Opzionale: pulizia delle vecchie cache
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
